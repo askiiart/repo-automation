b455279b2e6fcc817e067e25a7be5fd0a6dbb0d2
askiiart <dev@askiiart.net>
2023-09-11 22:31:06 -0500

    Disable Steam updating

--- debian.sh
+++ debian.sh
@@ -6,15 +6,22 @@ apt-get install -y dpkg-dev
 
 # Update steam - have to do all this stuff because the filename is always the same.
 # There's probably a better way to do this.
-curl -LO https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
-if ! cmp --silent "./steam.deb" "/repo/steam.deb"; then
-    curl -L https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -o /repo/steam.deb
-fi
+# Update: This doesn't actually work for some reason?
+#curl -LO https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
+#if ! cmp --silent "./steam.deb" "/repo/steam.deb"; then
+#    curl -L https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -o /repo/steam.deb
+#fi
 
 # Update ArmCord
 cd /repo
 curl -LO $(curl -s https://api.github.com/repos/ArmCord/ArmCord/releases/latest | grep "browser_download_url.*ArmCord_.*_amd64.deb" | cut -d : -f 2,3 | tr -d \") -C -
 cd /drone/src
+
+# Update th-ch/youtube-music
+# curl throws errors, will figure it out later
+#cd /repo
+#curl -LO $(curl -s https://api.github.com/repos/th-ch/youtube-music/releases/latest | grep "browser_download_url.*youtube-music_.*_amd64.deb" | cut -d : -f 2,3 | tr -d \") -C -
+#cd /drone/src
 
 # Do repo stuff
 dpkg-scanpackages --arch amd64 /repo > /repo/Packages
